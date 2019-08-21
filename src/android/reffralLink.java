d5bfb32cfcab7fa5a9cc7f58c8593da8e6dfe5ab
Ashad <ashad.zaman@vertex.lt>
2019-08-21 19:21:25 +0600

    apply broadcaster

--- src/android/reffralLink.java
+++ src/android/reffralLink.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class reffralLink extends CordovaPlugin {
-
+    private BroadcastReceiver mReceiver = null;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
@@ -19,6 +19,7 @@ public class reffralLink extends CordovaPlugin {
             this.coolMethod(message, callbackContext);
             return true;
         }
+        mReceiver = new BarcodeReceiver(callbackContext);
         return false;
     }
 
@@ -30,3 +31,32 @@ public class reffralLink extends CordovaPlugin {
         }
     }
 }
+public class BarcodeReceiver extends BroadcastReceiver {
+
+    private CallbackContext callbackContext;
+
+    public BarcodeReceiver (CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
+
+    public void onReceive(Context ctx, Intent intent) {
+
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            String referrerString = extras.getString("referrer");
+            if (referrerString != null) {
+                callbackContext.success(referrerString);
+                // SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+                // Editor edit = sharedPreferences.edit();
+                // edit.putString("referrer", referrerString);
+                // edit.commit();
+            }
+        }
+
+        // if (intent.getAction().equals(ACTION_BARCODE_SERVICE_BROADCAST)) {
+        //     strBarcode = intent.getExtras().getString(KEY_BARCODE_STR);
+        //     callbackContext.success(strBarcode);
+        // }
+    }
+} 
